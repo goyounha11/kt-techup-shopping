52858257055d5aa3c7a0fbc0c3326c649edcee14
goyounha11 <goyounha11@naver.com>
2025-11-28 11:09:45 +0900

    test: update assertion for foundedOrder to check for presence

--- src/test/java/com/kt/service/OrderServiceTest.java
+++ src/test/java/com/kt/service/OrderServiceTest.java
@@ -102,7 +102,7 @@ class OrderServiceTest {
 		var foundedOrder = orderRepository.findAll().stream().findFirst();
 
 		assertThat(foundedProduct.getStock()).isEqualTo(8L);
-		assertThat(foundedOrder).isEmpty();
+		assertThat(foundedOrder).isPresent();
 	}
 
 	@Test
